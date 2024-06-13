cb068d42f46a10910e085e37c35e2915e6d6e0ee
maryammehrabani <mehrbanmary@gmail.com>
2024-06-13 14:33:49 +0330

    Merge remote-tracking branch 'origin/master' into master

---
+++ DATA/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:87bc80384a525eeacd046a91649effb2026d3128029512b7dc8bfdaa812a1989
+size 415
---
+++ DATA/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21c749a25bee579e6864e68af1b6b12b0de36875e432374f3b57bcd67544ff76
+size 14127
